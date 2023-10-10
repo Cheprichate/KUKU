81da78744e56b77db656e0411a776f12c3a9f372
Cheprichate <petrovich15071998@mail.ru>
2023-10-10 11:12:09 +0400

    Добавьте файлы проекта.

---
+++ KUKU.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+int hours;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    std::cout << "Который сейчас час?\n";
+    std::cin >> hours;
+
+    if (hours < 1 || hours > 24) {
+        std::cout << "не корректное время\n";
+    }
+    else {
+        while (hours != 0) {
+            std::cout << "Ку-ку\n";
+            --hours;
+            if (hours > 12 && hours <= 24) {
+                hours -= 12;
+            }
+        }
+    }
+}
